8c6d37f12df7fa5cfc159c0aff9e8c73e4e23d6c
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2024-02-06 21:34:02 +0530

    project updated with index

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useSession } from 'next-auth/react'
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 export default function Home() {
+  const {data:session} = useSession();
+  const router = useRouter(); 
+
+  useEffect(()=>{
+    if(!session){
+      router.push('/login')
+    }
+  },[session])  
   return (
     <div className={styles.container}>
       

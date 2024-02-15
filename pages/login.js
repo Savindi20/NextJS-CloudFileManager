3b68caed872c04c456449346ad4e48d28c5d4b69
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2024-02-15 17:01:54 +0530

    project refactored with login

--- pages/login.js
+++ pages/login.js
@@ -1,32 +1,38 @@
 import { signIn, useSession } from 'next-auth/react'
-import { useEffect } from 'react';
+import Image from 'next/image';
 import { useRouter } from 'next/router';
-import React from 'react'
+import React, { useEffect } from 'react'
 
-function login() {
-  const {data:session}=useSession();
-  const router=useRouter();
-
-  useEffect(()=>{
-    console.log("User Session",)
-    if(!session)
-    {
-      router.push("/")
-    }
-    else{
-      console.log("User Session",session.user)
-    }
-
-  },[session])
-
+function Login() {
+    const {data:session}=useSession();
+    const router=useRouter();
+    useEffect(()=>{
+      console.log("User Session",)
+      if(session)
+      {
+        router.push("/")
+      }
+     
+    },[session])
   return (
     <div className='flex justify-center 
     items-center mt-[25%] ml-[0%] md:ml-[50%] flex-col gap-6'>
-        <button className='bg-blue-400 p-2 rounded-xl text-white'onClick={()=>signIn()}>
-          Sign with Google
-        </button>
+      <Image src='/logo.png'
+      alt='logo'
+      width={200}
+      height={100}
+      />
+        <button 
+        className=' text-white'
+        onClick={()=>signIn()}>
+            <Image src='/google.png'
+            alt='google'
+            width={300}
+            height={300}
+            />
+            </button>
     </div>
   )
 }
 
-export default login
+export default Login
